b464d5dc194d6cbfc52d54cd816f13b8d0acaeb3
ifechukwudei <nwokoifechukwu@gmail.com>
2023-01-01 15:52:38 +0100

    new features

--- js/index.js
+++ js/index.js
@@ -23,3 +23,59 @@ When one of these links is clicked, the event object is passed to the event list
 The querySelector function is used to select the target element specified in the link's href attribute. The getBoundingClientRect method is used to get the top position of the target element relative to the viewport.
 
 The scrollTo function is used to smoothly scroll the page to the target element. The top value specifies the vertical position to scroll to, and the behavior option is set to smooth to enable smooth scrolling. The duration and easing options can be used to customize the scroll behavior further. */
+const backToTopButton = document.getElementById("back-to-top-button")
+const footer = document.getElementById("footer")
+
+window.addEventListener("scroll", scrollFunction)
+
+function scrollFunction() {
+  const footerTop = footer.getBoundingClientRect().top
+  if (window.pageYOffset > 300 && footerTop > window.innerHeight) {
+    // Show back to top button if window is scrolled down 300px and footer is not in view
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit")
+      backToTopButton.classList.add("btnEntrance")
+      backToTopButton.style.display = "block"
+    }
+  } else {
+    // Hide back to top button if window is not scrolled down 300px or footer is in view
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance")
+      backToTopButton.classList.add("btnExit")
+      setTimeout(function () {
+        backToTopButton.style.display = "none"
+      }, 250)
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop)
+
+// function for smooth scroll back to top
+function smoothScrollBackToTop() {
+  const targetPosition = 0
+  const startPosition = window.pageYOffset
+  const distance = targetPosition - startPosition
+  const duration = 750
+  let start = null
+
+  window.requestAnimationFrame(step)
+
+  function step(timestamp) {
+    if (!start) start = timestamp
+    const progress = timestamp - start
+    window.scrollTo(
+      0,
+      easeInOutCubic(progress, startPosition, distance, duration)
+    )
+    if (progress < duration) window.requestAnimationFrame(step)
+  }
+}
+
+// function for scroll easing
+function easeInOutCubic(t, b, c, d) {
+  t /= d / 2
+  if (t < 1) return (c / 2) * t * t * t + b
+  t -= 2
+  return (c / 2) * (t * t * t + 2) + b
+}
